186be4b41bc48b239372c35646bbefe8eb899f0d
shipherman <aleksandr.tyagni@gmail.com>
2023-05-20 22:46:02 +0300

    Initial commit

---
+++ cmd/skill/main.go
@@ -0,0 +1,75 @@
+// пакеты исполняемых приложений должны называться main
+package main
+
+import (
+    "net/http"
+	"encoding/json"
+    "alice-skill/internal/logger"
+	"alice-skill/internal/models"
+
+	"go.uber.org/zap"
+)
+
+
+// функция main вызывается автоматически при запуске приложения
+func main() {
+	    parseFlags()
+
+    if err := run(); err != nil {
+        panic(err)
+    }
+}
+
+// функция run будет полезна при инициализации зависимостей сервера перед запуском
+func run() error {
+    if err := logger.Initialize(flagLogLevel); err != nil {
+        return err
+    }
+    
+    logger.Log.Info("Running server", zap.String("address", flagRunAddr))
+    // оборачиваем хендлер webhook в middleware с логированием
+    return http.ListenAndServe(flagRunAddr, logger.RequestLogger(webhook))
+}
+
+func webhook(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        return
+    }
+    
+    // десериализуем запрос в структуру модели
+    logger.Log.Debug("decoding request")
+    var req models.Request
+    dec := json.NewDecoder(r.Body)
+    if err := dec.Decode(&req); err != nil {
+        logger.Log.Debug("cannot decode request JSON body", zap.Error(err))
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    
+    // проверяем, что пришёл запрос понятного типа
+    if req.Request.Type != models.TypeSimpleUtterance {
+        logger.Log.Debug("unsupported request type", zap.String("type", req.Request.Type))
+        w.WriteHeader(http.StatusUnprocessableEntity)
+        return
+    }
+    
+    // заполняем модель ответа
+    resp := models.Response{
+        Response: models.ResponsePayload{
+            Text: "Извините, я пока ничего не умею",
+        },
+        Version: "1.0",
+    }
+    
+    w.Header().Set("Content-Type", "application/json")
+    
+    // сериализуем ответ сервера
+    enc := json.NewEncoder(w)
+    if err := enc.Encode(resp); err != nil {
+        logger.Log.Debug("error encoding response", zap.Error(err))
+        return
+    }
+    logger.Log.Debug("sending HTTP 200 response")
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module alice-skill
+
+go 1.19
+
+require (
+	github.com/go-resty/resty/v2 v2.7.0
+	github.com/stretchr/testify v1.8.2
+	go.uber.org/zap v1.24.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
